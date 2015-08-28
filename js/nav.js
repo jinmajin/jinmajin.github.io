ae168b2caf9437b74973927a6a070d7d28fddcd9
Mary Ann Jin <maryann.jin@gmail.com>
2015-08-28 19:21:31 -0400

    revamp

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,10 @@
 function navTo(page){
-	var mainDivs = document.querySelectorAll("#content .main");
-	for(var i = 0; i < mainDivs.length; i++){
-		mainDivs[i].style.display = "none";
-	}
-	var menuItems = document.querySelectorAll("#header .menuitem");
-	for(var i = 0; i < menuItems.length; i++){
-		menuItems[i].className = "menuitem";
-	}
-	document.getElementById(page).style.display = "block";
-	document.getElementById(page + "_item").className = "menuitem active";
+	$('#content .main').hide();
+	$('#header .menuitem').removeClass('active');
+	$('#'+ page).show();
+	$('#'+ page + '_item').addClass('active');
+}
+
+function toggleSlide(section) {
+	$(section).find('.project_info').slideToggle();
 }
